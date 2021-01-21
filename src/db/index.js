85cad12521c83bdc779f58828e1fea00a4f3fbe0
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-01-21 18:38:41 +0100

    ok

--- src/db/index.js
+++ src/db/index.js
@@ -1,1 +1,11 @@
 const { Sequelize, Datatype } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
+  {
+    host: env.process.PGHOST,
+    dialect: "postgres",
+  }
+);
